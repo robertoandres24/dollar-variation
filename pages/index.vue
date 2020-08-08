70e7c0de7d22f0c0898266c68cd43dfdff3a20c9
robertoandres24 <robertoandres24@gmail.com>
2020-08-08 16:45:46 -0400

    creates nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        usd-variation
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
