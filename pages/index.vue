cfe7214d92de643a5bdbc50b9f70485bfd9864ce
robertoandres24 <robertoandres24@gmail.com>
2020-08-08 22:40:15 -0400

    display variations in a line chart. first impressions

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,11 @@
       <h1 class="title">
         usd-variation
       </h1>
+      <line-chart v-if="loaded" :chartdata="chartdata" :options="options" />
+    </div>
+    <div v-if="variations.length">
+      <h3>Variations</h3>
+      <p>{{ variations }}</p>
     </div>
   </div>
 </template>
@@ -16,7 +21,10 @@ export default {
       currency: 'USDCLP',
       startDate: '2019-01-01',
       endDate: '2020-08-01',
+      options: null,
+      chartdata: null,
       variations: [],
+      loaded: false,
     }
   },
 
@@ -24,12 +32,53 @@ export default {
     const { data: timeserie } = await this.$axios.get(
       `https://marketdata.tradermade.com/api/v1/timeseries?api_key=${this.apiKey}&currency=${this.currency}&start_date=${this.startDate}&end_date=${this.endDate}`
     )
-    this.variations = timeserie.quotes
-      .map((clpDayValue) => parseInt(clpDayValue.close))
-      .map((curr, i, arr) => {
-        if (i === 0) return 0
-        return curr - arr[i - 1]
-      })
+    this.variations = await this.getVariations(timeserie.quotes)
+    this.fillChartData()
+  },
+
+  methods: {
+    getVariations(clpValuesInfo) {
+      return Promise.all(
+        clpValuesInfo
+          .map((clpDayValue) => parseInt(clpDayValue.close))
+          .map((curr, i, arr) => {
+            if (i === 0) return 0
+            return curr - arr[i - 1]
+          })
+      )
+    },
+    fillChartData() {
+      this.loaded = true
+      const vm = this
+      vm.chartdata = {
+        labels: vm.variations,
+        datasets: [
+          {
+            label: 'Dollar variation',
+            data: vm.variations,
+          },
+        ],
+      }
+      vm.options = {
+        responsive: true,
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: 'Dollar Variation',
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      }
+    },
   },
 }
 </script>
