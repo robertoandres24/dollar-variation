0d69129abcedafdd020b549f72f259c580ba4edd
robertoandres24 <robertoandres24@gmail.com>
2020-08-08 20:52:21 -0400

    get variations from 2019-2020

--- pages/index.vue
+++ pages/index.vue
@@ -10,11 +10,25 @@
 
 <script>
 export default {
+  data() {
+    return {
+      apiKey: 'XcHFrKnvEQgMeSPsEjfR',
+      currency: 'USDCLP',
+      startDate: '2019-01-01',
+      endDate: '2020-08-01',
+      variations: [],
+    }
+  },
+
   async created() {
-    const { data: todo } = await this.$axios.get(
-      'https://jsonplaceholder.typicode.com/todos/1'
+    const { data: timeserie } = await this.$axios.get(
+      `https://marketdata.tradermade.com/api/v1/timeseries?api_key=${this.apiKey}&currency=${this.currency}&start_date=${this.startDate}&end_date=${this.endDate}`
     )
-    console.log(todo)
+    const values = timeserie.quotes.map((q) => parseInt(q.close))
+    this.variations = values.map((curr, i, arr) => {
+      if (i === 0) return 0
+      return curr - arr[i - 1]
+    })
   },
 }
 </script>
